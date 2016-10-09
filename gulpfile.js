0e7a7de67487ea960cd0c90dcee56d6b4da09bd5
Tigran Atayan <tiko.atayan@gmail.com>
2016-10-09 19:18:38 +0400

    Fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,8 @@ var gulp = require( 'gulp' ),
 	gzip = require( 'gulp-gzip' ), 
 	uglify = require( 'gulp-uglify' ), 
 	rename = require( 'gulp-rename' ), 
-	concat = require( 'gulp-concat' ), 
 	moment = require( 'moment' ), 
 	header = require( 'gulp-header' ), 
-	bower = require( 'main-bower-files' ), 
 
 	pkg = require('./package.json'),
 	fs = require('fs'),
@@ -52,18 +50,23 @@ var gulp = require( 'gulp' ),
 		}
 	};
 
-gulp.task( 'minify', function () {
+gulp.task( 'minify', function ( done ) {
 	gulp.src( Config.filePath )
 		.pipe( uglify( uglify_settings ) )
 		.pipe( rename( { extname: '.min.js' } ) )
-		.pipe( gulp.dest( dist_folder ) );
+		.pipe( gulp.dest( Config.dist ) )
+		.on( 'end', done );
 } );
-gulp.task( 'gzipify', function () {
+
+//	This task must be executed after minify.
+gulp.task( 'gzipify', ['minify'], function () {
 	gulp.src( Config.dist + '*.min.js' )
 		.pipe( gzip() )
-		.pipe( gulp.dest( dist_folder ) );
+		.pipe( gulp.dest( Config.dist ) );
 } );
-gulp.task( 'addheader', function () {
+
+//	This task must be executed after minify.
+gulp.task( 'addheader', ['minify'], function () {
 	var file = fs.readFileSync( Config.minFilePath ).toString();
 	file = file.replace(/^\/\*(.|\n)+\*\//, '');
 	fs.writeFileSync( Config.minFilePath, file );
@@ -91,10 +94,10 @@ gulp.task( 'addheader', function () {
 			].join( '' ), 
 			header_options 
 		) )
-		.pipe( gulp.dest( dist_folder ) );
+		.pipe( gulp.dest( Config.dist ) );
 } );
 
 gulp.task( 'default', [], function () {
-	gulp.watch( [ filepath ], [ 'minify', 'gzipify', 'addheader' ] );
-	gulp.start( [ 'minify', 'gzipify', 'addheader' ] );
+	gulp.watch( [ Config.filePath ], [ 'minify', 'addheader', 'gzipify' ] );
+	gulp.start( [ 'minify', 'addheader', 'gzipify' ] );
 } );
